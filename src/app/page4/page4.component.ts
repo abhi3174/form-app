bc37f8989deb25e3d8f9fd5d124fcd1becf863ae
koderoom <koderoom.dev@gmail.com>
2020-09-02 11:38:32 +0530

    page.4

--- src/app/page4/page4.component.ts
+++ src/app/page4/page4.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-page4',
   templateUrl: './page4.component.html',
-  styleUrls: ['./page4.component.css']
+  styleUrls: ['./page4.component.css'],
 })
 export class Page4Component implements OnInit {
+  // constructor() {}
+  // constructor(private router: Router) {}
+  // constructor(private router: Router, private http: HttpClient) {}
+  // constructor(private http: HttpClient, private router: Router) {}
 
-  constructor() { }
+  // Injecton :: Dependency Injection
+  /*private http: HttpClient;
+  constructor(http: HttpClient) {
+    this.http = http;
+  }*/
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
