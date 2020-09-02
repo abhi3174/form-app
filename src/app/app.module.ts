4cbd0a3335e8bd5cf55d4e804759bdc0780d799d
koderoom <koderoom.dev@gmail.com>
2020-09-02 10:34:30 +0530

    page3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
-  declarations: [AppComponent, Page1Component, Page2Component],
+  declarations: [AppComponent, Page1Component, Page2Component, Page3Component],
   imports: [
     BrowserModule,
     AppRoutingModule,
