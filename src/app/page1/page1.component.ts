baa0e20edf597cf49a0177785439f64ee0ba7236
koderoom <koderoom.dev@gmail.com>
2020-09-02 10:15:28 +0530

    multi.field.forms

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  styleUrls: ['./page1.component.css'],
 })
 export class Page1Component implements OnInit {
+  myFormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
